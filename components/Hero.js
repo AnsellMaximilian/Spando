9aa28c61eda56912f785e8c8d48f7589c82a21b3
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-09-21 15:57:01 +0700

    setup and hero section

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="relative bg-right-top bg-no-repeat bg-contain h-60 text-center md:text-left lg:h-125 md:bg-hero-md">
+      <div className="container h-full mx-auto p-4 flex flex-col justify-center">
+        <h1 className="text-5xl font-bold md:w-2/3 lg:w-1/2">
+          Belajar <span className="text-red-700">Sp</span>
+          <span className="text-yellow-400">any</span>
+          <span className="text-red-700">ol</span> dari sudut pandang pemula.
+        </h1>
+      </div>
+    </section>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-md": "url('/images/hero-md.svg')",
+      },
+      spacing: {
+        125: "31.25rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
