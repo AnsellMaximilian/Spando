30a8463e09374ea2d0a6dd608e90d9b98c1728a0
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-11-17 18:21:53 +0700

    tags page

--- pages/tags/index.js
+++ pages/tags/index.js
@@ -23,25 +23,25 @@ export default function PostList({ tags }) {
   return (
     <Layout tags>
       <div className="container px-4 mx-auto mt-4 grid grid-cols-12 gap-4">
-        <Panel classes="col-span-12">
-          <main className="grid grid-cols-12 gap-1 bg-gray-200">
-            {tags.map((tag) => (
-              <article
-                key={tag.sys.id}
-                className="p-4 hover:bg-gray-100 group col-span-12 bg-white md:col-span-4"
-              >
-                <Link href={`/tags/${tag.fields.slug}`}>
-                  <a className="block">
-                    <h2 className="font-bold group-hover:text-red-600">
+        <main className="col-span-12 grid grid-cols-12 gap-4">
+          {tags.map((tag) => (
+            <Panel
+              key={tag.sys.id}
+              classes="md:col-span-4 col-span-12 group hover:bg-gray-100 flex flex-col"
+            >
+              <Link href={`/tags/${tag.fields.slug}`}>
+                <a className="block flex-1">
+                  <article className="p-4">
+                    <h2 className="font-bold group-hover:text-red-600 text-xl">
                       {tag.fields.name}
                     </h2>
                     <p>{tag.fields.description}</p>
-                  </a>
-                </Link>
-              </article>
-            ))}
-          </main>
-        </Panel>
+                  </article>
+                </a>
+              </Link>
+            </Panel>
+          ))}
+        </main>
         {/* <Panel classes="col-span-12 lg:col-span-4">
           <aside></aside>
         </Panel> */}
