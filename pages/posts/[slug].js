cb5e1b1011a34e76dee18da0039d628153687cd2
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-07 13:16:10 +0700

    Accomodate contentful structure change for similar posts

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -25,11 +25,6 @@ export async function getStaticProps({ params }) {
     props: {
       post: mainPost,
       tags: tags.items,
-      similarPosts: posts.items.filter(
-        (post) =>
-          post.fields.slug !== params.slug &&
-          post.fields.tags.some((tag) => mainPost.fields.tags.includes(tag))
-      ),
     },
   };
 }
@@ -46,7 +41,8 @@ async function getPostsAndTags() {
   return { posts, tags };
 }
 
-export default function Home({ post, tags, similarPosts }) {
+export default function Home({ post }) {
+  console.log(post.fields.similarPosts);
   return (
     <Layout posts>
       <div className="container mx-auto px-4 grid grid-cols-12 gap-4 mt-8">
@@ -54,15 +50,15 @@ export default function Home({ post, tags, similarPosts }) {
           <Post post={post} />
         </main>
         <aside className="col-span-12 md:col-span-4">
-          <Panel classes="sticky top-4">
-            {similarPosts.length > 0 && (
+          {post.fields.similarPosts && (
+            <Panel classes="sticky top-4">
               <PostLinkList
-                posts={similarPosts}
+                posts={post.fields.similarPosts}
                 listTitle="Mirip"
                 listItemClasses="col-span-12"
               />
-            )}
-          </Panel>
+            </Panel>
+          )}
         </aside>
       </div>
     </Layout>
