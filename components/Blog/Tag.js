7d58c01b51be39cd481002cfe40f2e7404183811
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-07 16:48:58 +0700

    tag page

--- components/Blog/Tag.js
+++ components/Blog/Tag.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
-export default function Tag({ variant, tag }) {
+export default function Tag({ variant, tag, active }) {
   const additionalClasses =
     variant === "big" ? "px-4 py-2 text-lg" : "px-2 py-1";
   return (
     <Link href={`/tags/${tag.fields.slug}`}>
       <a
-        className={`bg-gray-100 px-2 py-1 rounded-md text-gray-600 text-sm hover:bg-gray-200 hover:text-black border border-gray-100 hover:border-gray-300 ${additionalClasses}`}
+        className={`px-2 py-1 rounded-md  ${
+          active
+            ? "text-black border bg-gray-200 border-gray-300"
+            : "bg-gray-100 text-gray-600 border-gray-100"
+        }  text-sm hover:bg-gray-200 hover:text-black border hover:border-gray-300 ${additionalClasses}`}
       >
         {tag.fields.name}
       </a>
--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 import client from "../../contentful";
 
 import Layout from "../../components/Layout";
 import Panel from "../../components/Panel";
-import PostLink from "../../components/Blog/PostLink";
 import PostCard from "../../components/Blog/PostCard";
+import Tag from "../../components/Blog/Tag";
 
 export async function getStaticPaths() {
-  const { tags } = await getPostsAndTags();
+  const tags = await client.getEntries({
+    content_type: "tag",
+  });
 
   const paths = tags.items.map((tag) => ({
     params: { slug: tag.fields.slug },
@@ -16,20 +20,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { posts, tags } = await getPostsAndTags();
-  return {
-    props: {
-      tags: tags.items,
-      posts: posts.items.filter(
-        (post) =>
-          post.fields.tags.filter((tag) => tag.fields.slug === params.slug)
-            .length > 0
-      ),
-    },
-  };
-}
-
-async function getPostsAndTags() {
   const posts = await client.getEntries({
     content_type: "blogPost",
   });
@@ -38,34 +28,60 @@ async function getPostsAndTags() {
     content_type: "tag",
   });
 
-  return { posts, tags };
+  const tag = tags.items.find((tag) => tag.fields.slug === params.slug);
+  return {
+    props: {
+      slug: params.slug,
+      tags: tags.items,
+      tag,
+      posts: posts.items.filter((post) =>
+        post.fields.tags.some((tag) => tag.fields.slug === params.slug)
+      ),
+    },
+  };
 }
 
-export default function Home({ posts, tags }) {
+export default function Home({ posts, tag, tags, slug }) {
   return (
     <Layout posts>
       <div className="container px-4 mx-auto mt-4 grid grid-cols-12 gap-4">
-        <div className="col-span-12">
-          <h1 className="text-4xl font-bold text-center">
-            Browsing by tag: Terjemahan
-          </h1>
-        </div>
+        <Panel classes="col-span-12 text-center">
+          <div className="p-4">
+            <h1 className="text-2xl font-bold mb-2">
+              Menelusuri Tag {tag.fields.name}
+            </h1>
+            <p>{tag.fields.description}</p>
+          </div>
+          <div className="border-t-2 border-gray-200 p-4">
+            {/* <h3 className="font-semibold mb-2">Semua Tag</h3> */}
+            <div className="flex gap-4 flex-wrap">
+              {tags.map((tag) => (
+                <Tag
+                  tag={tag}
+                  key={tag.sys.id}
+                  active={slug === tag.fields.slug}
+                />
+              ))}
+            </div>
+          </div>
+        </Panel>
         {posts.length > 0 ? (
-          <main className="grid grid-cols-12 gap-4 col-span-12 lg:col-span-8">
+          <main className="grid grid-cols-12 gap-4 col-span-12">
             {posts.map((post, index) => {
               const {
                 sys: { id },
                 fields: { title, excerpt, publishDate: date, tags, slug },
               } = post;
               return (
-                <PostCard
-                  key={id}
-                  title={title}
-                  slug={slug}
-                  excerpt={excerpt}
-                  date={date}
-                  tags={tags}
-                />
+                <Panel key={id} classes="col-span-12 md:col-span-4">
+                  <PostCard
+                    title={title}
+                    slug={slug}
+                    excerpt={excerpt}
+                    date={date}
+                    tags={tags}
+                  />{" "}
+                </Panel>
               );
             })}
           </main>
@@ -74,9 +90,6 @@ export default function Home({ posts, tags }) {
             Sorry, there are no more posts.
           </div>
         )}
-        <Panel classes="col-span-12 lg:col-span-4">
-          <aside></aside>
-        </Panel>
       </div>
     </Layout>
   );
