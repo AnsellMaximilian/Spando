24a82b62319e7c31f2db35fa4d0c4444d2f24230
Ansell Maximilian <56351143+AnsellMaximilian@users.noreply.github.com>
2023-04-05 20:05:06 +0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -5,9 +5,9 @@ export default function Hero() {
     <section className="relative h-60 text-center md:text-left lg:h-125">
       <div className="container h-full mx-auto p-4 flex flex-col justify-center bg-right-top bg-no-repeat bg-contain md:bg-hero-md">
         <h1 className="text-5xl font-bold md:w-2/3 lg:w-1/2">
-          Belajar <span className="text-red-700">Sp</span>
+          Blog Bahasa <span className="text-red-700">Sp</span>
           <span className="text-yellow-400">any</span>
-          <span className="text-red-700">ol</span> dari sudut pandang pemula.
+          <span className="text-red-700">ol</span>.
         </h1>
       </div>
     </section>
